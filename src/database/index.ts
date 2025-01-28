3fb1ad2f4f8e4bf80ff6456d6e7365d1e865eac8
Balakannan <balakannan2522@gmail.com>
2025-01-28 20:55:31 +0530

    fix: mongo_url

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,9 @@
 import { connect, set } from 'mongoose';
-import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { NODE_ENV, MONGO_URL } from '@config';
 
 export const dbConnection = async () => {
   const dbConfig = {
-    url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+    url: MONGO_URL,
   };
 
   if (NODE_ENV !== 'production') {
